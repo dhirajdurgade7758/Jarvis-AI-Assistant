cc3a5b305da14ec8a326f9256920922f226e8c8d
Dhiraj <dhirajdurgade7758@gmail.com>
2025-02-20 21:28:55 +0530

    first commit

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "triple og":"https://www.youtube.com/watch?v=pqZuSz8_2DM",
+    "russian bandana":"https://www.youtube.com/watch?v=1OAjeECW90E&list=RDpqZuSz8_2DM&index=3",
+    "victory anthem":"https://www.youtube.com/watch?v=0hJ4F-Nj5VY&list=RDpqZuSz8_2DM&index=2",
+    "hanuman kind":"https://www.youtube.com/watch?v=hOHKltAiKXQ&list=RDpqZuSz8_2DM&index=6",
+    "check it out":"https://www.youtube.com/watch?v=RiZL2j5mIPw&list=RDpqZuSz8_2DM&index=4"
+
+}
